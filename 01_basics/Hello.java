01626f6c5d7c9378fa2ea1a816f571844f150f88
Debastuti07 <debastutide@gmail.com>
2026-09-03 11:04:56 +0530

    java basics

---
+++ 01_basics/Hello.java
@@ -0,0 +1,9 @@
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java !");
+        System.out.print("Hello megha !\n");
+        System.out.println("Hello megha\n and tuti !");
+    }
+}
