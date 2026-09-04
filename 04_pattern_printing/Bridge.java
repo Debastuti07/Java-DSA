e817129e633891cd5762abb3a74dc7c88942e3fd
Debastuti07 <debastutide@gmail.com>
2026-09-05 02:07:53 +0530

    Pattern printing

---
+++ 04_pattern_printing/Bridge.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Bridge {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= 2 * n - 1; j++) {
+
+                if (j <= n + 1 - i || j >= n - 1 + i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+}
+}
