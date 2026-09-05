ae4245b7ef87cb3c29a23bd7fad24c20225d56af
Debastuti07 <debastutide@gmail.com>
2026-09-06 01:33:17 +0530

    loops

---
+++ 03_loops/ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        //use to travarse elements 
+        // cannot modify elements 
+
+        int[]arr={5,8,2,3,9};
+        for(int element:arr)
+            System.out.print(2*element+" ");
+    }
+}
