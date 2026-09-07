8723ddde892a2eb861b077356a983477126453fd
Debastuti07 <debastutide@gmail.com>
2026-09-07 12:56:11 +0530

    Sorting

--- 08_sorting(part1)/ReverseBubbleSort.java
+++ 08_sorting(part1)/ReverseBubbleSort.java
@@ -3,14 +3,14 @@ public class ReverseBubbleSort {
     public static void main(String[] args) {
          int[]arr={5,-2,6,7,2,0,7,2};
          for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length-1;j++){
+            for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]<arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
 
-
+ 
             }
 
          }
