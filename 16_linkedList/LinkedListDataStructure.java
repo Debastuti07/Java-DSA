5541a841f0380f5d462f469f4e6350ae3c20e0b6
Debastuti07 <debastutide@gmail.com>
2026-09-23 02:12:31 +0530

    Linked List

--- 16_linkedList/LinkedListDataStructure.java
+++ 16_linkedList/LinkedListDataStructure.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 class Node{ //user defined data type 
     int val;
     Node next;
+    
 
     Node(int val){
         this.val=val;
@@ -12,16 +13,23 @@ class Node{ //user defined data type
 class LL{//user defined data structure
     Node head;
     Node tail;
-//add at the end 
-    void addAtTail(int x){
-        Node temp=new Node(x);
-        if(tail==null) {
-            head=tail=temp;
-            return;
+    int size;
+
+    int  search(int val){
+         if(head==null) return -1;
+        Node temp=head;
+        int idx=0;
+        while(temp!=null){
+            if(temp.val==val){
+                return idx;
+            }
+            temp=temp.next;
+            idx++;
         }
-        tail.next=temp;
-        tail=temp;
+        return -1;
     }
+
+
     void display(){
         if(head==null) return;
         Node temp=head;
@@ -31,18 +39,66 @@ class LL{//user defined data structure
         }
         System.out.println();
     }
+
+
+//add at the end 
+    void addAtTail(int x){
+        Node temp=new Node(x);
+        if(tail==null) {
+            head=tail=temp;
+            size++;
+            return;
+        }
+        tail.next=temp;
+        tail=temp;
+        size++;
+    }
+
+
+
+
     //add at the beginning 
     void addAtHead(int x){
         Node temp=new Node(x);
         if(tail==null){
             head=tail=temp;
+            size++;
             return;
         }
         temp.next=head;
         head=temp;
+        size++;
     }
 
-    //
+
+    //delete at head
+    void deleteAtHead(){
+       
+        if(head==null) return;
+        head=head.next;
+        size--;
+    }
+
+
+    //delete from the end
+   void deleteFromEnd(){
+     if(head==null) return;
+     if(head==tail)
+     {
+        head=tail=null;
+        size--;
+        return ;
+     }
+    Node temp=head;
+    while(temp.next!=tail)
+    {
+        temp=temp.next;
+    }
+    temp.next=null;
+    tail=temp;
+    size--;
+
+   }
     
 }
 public class LinkedListDataStructure {
@@ -56,5 +112,13 @@ public class LinkedListDataStructure {
 
         l1.addAtHead(70);
         l1.display();
+        
+        l1.deleteAtHead();
+        l1.display();
+        
+        l1.deleteFromEnd();
+        l1.display();
+
+        System.out.println(l1.search(20));
     }
 }
